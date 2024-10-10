2842944cec53d3784b78850e61bc67e2fb64293e
Anna Carey <annacarey@Annas-MacBook-Air.local>
2024-10-10 10:21:17 +0100

    Fix button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -90,10 +90,14 @@
         <p>Upload video</p></Dropzone
       >
     </Button>
-    <Button on:click={handleDownload} isDisabled={!conversionState.pdfBytes}>
+    <button
+      class="flex gap-2 items-center justify-center bg-[#406AFF] text-[#DCDEE9] text-[32px] px-5 py-3 rounded-md disabled:bg-[#DCDEE9] disabled:text-[#406AFF] disabled:border-2 disabled:border-[#406AFF] disabled:cursor-not-allowed"
+      on:click={handleDownload}
+      disabled={!conversionState.pdfBytes}
+    >
       <p>Download</p>
       <img src={Download} alt="Download" />
-    </Button>
+    </button>
   </div>
   {#if conversionState.isError}
     <p class="text-red-500">There was an error. Please try again</p>
